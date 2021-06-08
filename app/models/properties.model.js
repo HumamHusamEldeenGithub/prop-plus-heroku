1dcf30a5a5c35078808c9df872e7c1a124f93e28
Humam-Husam-Eldeen <humamhusameldeen11@gmail.com>
2021-06-08 13:44:50 +0300

    log

--- app/models/properties.model.js
+++ app/models/properties.model.js
@@ -1,101 +1,108 @@
-const sql = require('./db') ; 
+const sql = require("./db");
 
-const Property = function(property){
-    this.name = property.name  , 
-    this.user_id = property.user_id , 
-    this.phone = property.phone , 
-    this.description = property.description ,
-    this.rating = property.rating 
-} ; 
+const Property = function (property) {
+  (this.name = property.name),
+    (this.user_id = property.user_id),
+    (this.phone = property.phone),
+    (this.description = property.description),
+    (this.rating = property.rating);
+};
 
 Property.create = (newProperty, result) => {
-    sql.query("INSERT INTO properties SET ?", newProperty, (err, res) => {
-        if (err) {
-            console.log(err);
-            result(err, null);
-            return;
-        }
-        console.log("created property !");
-        result(null,{ id: res.insertId, ...newProperty }) ; 
-    });
-}
+  sql.query("INSERT INTO properties SET ?", newProperty, (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    console.log("created property !");
+    result(null, { id: res.insertId, ...newProperty });
+  });
+};
 Property.findById = (propertyId, result) => {
-    sql.query('SELECT * FROM properties WHERE id = ' + propertyId, (err, res) => {
-        if (err) {
-            console.log(err);
-            result(err, null);
-            return;
-        }
-        if (res.length) {
-            console.log("found property : ", res[0]);
-            result(null, res[0]);
-            return;
-        }
-        result({ kind: "not_found" }, null);
-    });
-}
+  sql.query("SELECT * FROM properties WHERE id = " + propertyId, (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    if (res.length) {
+      console.log("found property : ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+    result({ kind: "not_found" }, null);
+  });
+};
 
-Property.getAll = result => {
-    sql.query("SELECT * FROM properties", (err, res) => {
-        if (err) {
-            console.log(err);
-            result(err, null);
-            return;
-        }
-        console.log("properties:", res);
-        result(null, res);
-    });
-}
-
+Property.getAll = (result) => {
+  sql.query("SELECT * FROM properties", (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    console.log("properties:", res);
+    result(null, res);
+  });
+};
 
 Property.updateById = (id, newProperty, result) => {
-    console.log(id) ; 
-    sql.query("UPDATE properties SET name= ? , phone = ? , description = ? , rating = ? WHERE id=?", [newProperty.name, newProperty.user_id,newProperty.phone,newProperty.description,newProperty.rating, id],
-        (err, res) => {
-            if (err) {
-                console.log(err);
-                result(err, null);
-                return;
-
-            }
-            //NOT FOUND
-            if (res.affectedRows == 0) {
-                result({ kind: "not_found" }, null);
-                return;
-            }
-            console.log("updated property :" + id);
-            result(null, { id: id, ...newProperty });
-        });
-}
+  console.log(id);
+  sql.query(
+    "UPDATE properties SET name= ? , phone = ? , description = ? , rating = ? WHERE id=?",
+    [
+      newProperty.name,
+      newProperty.phone,
+      newProperty.description,
+      newProperty.rating,
+      id
+    ],
+    (err, res) => {
+      if (err) {
+        console.log(err);
+        result(err, null);
+        return;
+      }
+      //NOT FOUND
+      if (res.affectedRows == 0) {
+        result({ kind: "not_found" }, null);
+        return;
+      }
+      console.log("updated property :" + id);
+      result(null, { id: id, ...newProperty });
+    }
+  );
+};
 
 Property.remove = (id, result) => {
-    sql.query("DELETE FROM properties WHERE id = ?", id, (err, res) => {
-        if (err) {
-            console.log(err);
-            result(err, null);
-            return;
-        }
-        //NOT FOUND 
-        if (res.affectedRows == 0) {
-            result({ kind: "not_found" }, null);
-            return;
-        }
-        console.log("deleted property with id =", id);
-        result(null, res);
-    });
-}
+    console.log(id) ; 
+  sql.query("DELETE FROM properties WHERE id = ?", id, (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    //NOT FOUND
+    if (res.affectedRows == 0) {
+      result({ kind: "not_found" }, null);
+      return;
+    }
+    console.log("deleted property with id =", id);
+    result(null, res);
+  });
+};
 
-Property.removeAll = result => {
-    sql.query("DELETE FROM proerties", (err, res) => {
-        if (err) {
-            console.log(err);
-            result(err, null);
-            return;
-        }
-        console.log("Deleted " + res.affectedRows + " property");
-        result(null, res);
-    })
-}
-
+Property.removeAll = (result) => {
+  sql.query("DELETE FROM proerties", (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    console.log("Deleted " + res.affectedRows + " property");
+    result(null, res);
+  });
+};
 
 module.exports = Property;
