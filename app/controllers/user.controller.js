6bc301c1f4516b71fefd1a838b7f61f601d5e63b
Humam-Husam-Eldeen <humamhusameldeen11@gmail.com>
2021-06-08 11:58:11 +0300

    Added an user id

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -9,6 +9,7 @@ exports.create = (req , res)=>{
         }) ; 
     }
     const user = new User ({
+        id:req.body.id,
         name : req.body.name , 
         email:req.body.email , 
         phone : req.body.phone , 
