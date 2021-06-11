c1daf241eff988ae1c916633de37b29c7284f13b
Humam-Husam-Eldeen <humamhusameldeen11@gmail.com>
2021-06-11 09:44:05 +0300

    Add paymnets

--- app/controllers/services.controllers.js
+++ app/controllers/services.controllers.js
@@ -7,7 +7,7 @@ exports.create = (req , res)=>{
         }) ; 
     }
     const service = new Service ({
-        property_id : req.body.propertyId , 
+        property_id : req.body.property_id , 
         description:req.body.description , 
         price_per_night : req.body.start_date 
     }) ; 
