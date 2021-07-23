eddaa39255a5810fe43d4644b6a507ab6af577b0
humam.husam.eldeen@gmail.com <humam.husam.eldeen@gmail.com>
2021-07-23 10:13:47 +0300

    CORS

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 require('dotenv').config();
-const express = require("express") ; 
-const bodyParser = require("body-parser") ; 
-const port = process.env.PORT ; 
+const express = require("express");
+const bodyParser = require("body-parser");
+const port = process.env.PORT;
+const cors = require('cors');
 
+const app = express();
 
-
-const app = express() ; 
+app.use(cors({
+    origin: "*",
+}));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.use(bodyParser.json()) ; 
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended:true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/" , (req,res)=>{
-    res.json({"message": "ok"}) ; 
-}); 
+app.get("/", (req, res) => {
+    res.json({ "message": "ok" });
+});
 
 require("./app/routes/tables.routes.js")(app);
 require("./app/routes/user.routes.js")(app);
-require('./app/routes/bookings.routes.js')(app) ; 
-require('./app/routes/services.routes.js')(app) ; 
-require('./app/routes/properties.routes.js')(app) ; 
-require('./app/routes/properties_to_approve.routes.js')(app) ; 
-require('./app/routes/payments.routes.js')(app) ; 
-require('./app/routes/locations.routes.js')(app) ; 
-require('./app/routes/images.routes.js')(app) ; 
-require('./app/routes/approval_images.routes.js')(app) ; 
+require('./app/routes/bookings.routes.js')(app);
+require('./app/routes/services.routes.js')(app);
+require('./app/routes/properties.routes.js')(app);
+require('./app/routes/properties_to_approve.routes.js')(app);
+require('./app/routes/payments.routes.js')(app);
+require('./app/routes/locations.routes.js')(app);
+require('./app/routes/images.routes.js')(app);
+require('./app/routes/approval_images.routes.js')(app);
 
-app.listen(port , ()=>{
-    console.log("Server is running on port " + port) ; 
-}) ; 
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
